6137fad2ebaa8b4e34d868b6ffb3376d0b04bcd4
Elvira Shelyganova <elvirashelyganova@gmail>
2021-07-26 10:04:46 +0200

    SearchResultMapScreen and PostCarouselItem

---
+++ visitOpatija/Shared/visitOpatijaApp.swift
@@ -0,0 +1,17 @@
+//
+//  visitOpatijaApp.swift
+//  Shared
+//
+//  Created by Эльвира Шелыганова on 25.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct visitOpatijaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
